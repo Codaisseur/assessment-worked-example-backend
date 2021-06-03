a5e3a0273cdb04baf6ce5c202fce612ea41b0261
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-06-03 10:53:53 +0200

    add point to post new bids

--- routers/artworks.js
+++ routers/artworks.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const User = require("../models/").user;
 const Artwork = require("../models").artwork;
 const Bid = require("../models").bid;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -29,7 +30,7 @@ router.get("/:id", async (req, res) => {
 
   const artwork = await Artwork.findByPk(id, {
     include: [Bid],
-    order: [[Bid, "createdAt", "DESC"]],
+    order: [[Bid, "createdAt", "ASC"]],
   });
 
   if (artwork === null) {
@@ -50,6 +51,46 @@ router.patch("/:id", async (req, res) => {
   await artwork.update({ hearts });
 
   return res.status(200).send({ artwork });
+});
+
+router.post("/:id/bids", authMiddleware, async (req, res) => {
+  const artwork = await Artwork.findByPk(req.params.id);
+
+  if (artwork === null) {
+    return res.status(404).send({ message: "This artwork does not exist" });
+  }
+
+  const bids = await Bid.findAll({
+    where: { artworkId: req.params.id },
+    // order: [[Bid, "createdAt", "DESC"]],
+  });
+  console.log(bids);
+  const lastMaxBid =
+    bids.length > 0 ? bids[bids.length - 1].amount + 1 : artwork.minimumBid;
+
+  /**The +1 here it's placed so I can use the same comparation
+   *  for the first bid (min value = to minimumbid) and the rest (min value > lastbid) */
+  const { amount } = req.body;
+  const email = req.user.email;
+  console.log(amount, email, lastMaxBid);
+
+  if (!email || !amount) {
+    return res
+      .status(400)
+      .send({ message: "A bid must have an asociated email and an amount" });
+  }
+
+  if (amount < lastMaxBid) {
+    return res.status(400).send({ message: "Your bid was too low" });
+  }
+
+  const bid = await Bid.create({
+    email,
+    amount,
+    artworkId: artwork.id,
+  });
+
+  return res.status(201).send({ message: "Bid created", bid });
 });
 
 module.exports = router;
--- sampleRequests/authorizedPost.js
+++ sampleRequests/authorizedPost.js
@@ -8,7 +8,7 @@ async function meExample() {
     // if you havent follow the setup steps from the readme first
     const responseLogin = await axios.post(`http://localhost:${PORT}/login`, {
       email: "test@test.com",
-      password: "test1234"
+      password: "test1234",
     });
     console.log("RESPONSE FROM SERVER", responseLogin.data);
 
@@ -21,12 +21,12 @@ async function meExample() {
       {
         // this is our request body
         title: "My new thing",
-        moreData: "I like turtles"
+        moreData: "I like turtles",
       },
       {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
 
@@ -37,4 +37,41 @@ async function meExample() {
   }
 }
 
-meExample();
+//meExample();
+async function bidPost() {
+  try {
+    // 1. first login to get a token
+    // this assumes you seeded the test user using sequelize cli seeds
+    // if you havent follow the setup steps from the readme first
+    const responseLogin = await axios.post(`http://localhost:${PORT}/login`, {
+      email: "test@test.com",
+      password: "test1234",
+    });
+    console.log("RESPONSE FROM SERVER", responseLogin.data);
+
+    const token = responseLogin.data.token;
+
+    // 2. now we can use the token in the /me endpoint to get
+    // the email & name of this user
+    const response = await axios.post(
+      `http://localhost:${PORT}/artworks/2/bids`,
+      {
+        // this is our request body
+
+        amount: 10,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.log("OH NO AN ERROR", error.message);
+    console.log("WHAT HAPPENED?", error.response.data);
+  }
+}
+
+bidPost();
