9265ce3142107872c93044e5ee5105420ffc7200
Matias Garcia <matiasigarcia91@gmail.com>
2021-06-04 16:12:04 +0200

    Add try/catch to all routes

--- routers/artworks.js
+++ routers/artworks.js
@@ -10,55 +10,66 @@ const router = new Router();
 
 router.get("/", async (req, res) => {
   // don't send back the password hash
-
-  const artworks = await Artwork.findAll({
-    include: [Bid],
-    order: [[Bid, "createdAt", "DESC"]],
-  });
-  console.log("when i fetch", artworks[1].hearts);
-  res.status(200).send({ message: "ok", artworks });
+  try {
+    const artworks = await Artwork.findAll({
+      include: [Bid],
+      order: [[Bid, "createdAt", "DESC"]],
+    });
+    console.log("when i fetch", artworks[1].hearts);
+    res.status(200).send({ message: "ok", artworks });
+  } catch(e) {
+    next(e)
+  }
 });
 
 router.post("/", authMiddleware, async (req, res) => {
-  const { title, imageUrl, minimumBid } = req.body;
-
-  console.log(10, title, imageUrl, minimumBid);
-
-  if (!title || !imageUrl || !minimumBid) {
-    return res
-      .status(400)
-      .send({ message: "A story must have a title, imageUrl and minimumBid" });
+  try {
+    const { title, imageUrl, minimumBid } = req.body;
+  
+    console.log(10, title, imageUrl, minimumBid);
+  
+    if (!title || !imageUrl || !minimumBid) {
+      return res
+        .status(400)
+        .send({ message: "A story must have a title, imageUrl and minimumBid" });
+    }
+    const artwork = await Artwork.create({
+      title,
+      imageUrl,
+      hearts: 0,
+      minimumBid,
+      userId: req.user.id,
+    });
+  
+    return res.status(201).send({ message: "Artwork created", artwork });
+  } catch(e) {
+    next(e)
   }
-  const artwork = await Artwork.create({
-    title,
-    imageUrl,
-    hearts: 0,
-    minimumBid,
-    userId: req.user.id,
-  });
-
-  return res.status(201).send({ message: "Artwork created", artwork });
 });
 
 /**Get arwork by id */
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  console.log(id);
-  if (isNaN(parseInt(id))) {
-    return res.status(400).send({ message: "artwork id is not a number" });
+  try {
+    const { id } = req.params;
+  
+    console.log(id);
+    if (isNaN(parseInt(id))) {
+      return res.status(400).send({ message: "artwork id is not a number" });
+    }
+  
+    const artwork = await Artwork.findByPk(id, {
+      include: [Bid],
+      order: [[Bid, "createdAt", "ASC"]],
+    });
+  
+    if (artwork === null) {
+      return res.status(404).send({ message: "artwork not found" });
+    }
+  
+    res.status(200).send({ message: "ok", artwork });
+  } catch(e) {
+    next(e)
   }
-
-  const artwork = await Artwork.findByPk(id, {
-    include: [Bid],
-    order: [[Bid, "createdAt", "ASC"]],
-  });
-
-  if (artwork === null) {
-    return res.status(404).send({ message: "artwork not found" });
-  }
-
-  res.status(200).send({ message: "ok", artwork });
 });
 
 /**Change the hearts */
@@ -111,7 +122,7 @@ router.post("/:id/bids", authMiddleware, async (req, res) => {
   });
 
   return res.status(201).send({ message: "Bid created", bid });
-  
+
   } catch(e) {
     next(e)
   }
